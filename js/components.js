6ad61788d3041806c383390d7e4ca23e6d228d61
Darikso27 <dariksodanielo@gmail.com>
2026-04-01 18:16:35 -0500

    modificacion de presentacion de redes sociales

--- js/components.js
+++ js/components.js
@@ -65,7 +65,7 @@ const footerHtml = `    <footer class="footer">
                 </div>
             </div>
             <div class="footer-social">
-                <h4>Síguenos</h4>
+                <h4>Síguenos en nuestras redes sociales</h4>
                 <div class="footer-social-icons">
                     <a href="https://youtube.com/@ekalmade.i.r.l?si=H10nqovH8kuvG5qv" target="_blank" rel="noopener noreferrer" class="social-icon social-youtube" aria-label="YouTube">
                         <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
